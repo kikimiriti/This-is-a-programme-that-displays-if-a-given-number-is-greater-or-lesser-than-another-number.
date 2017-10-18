7da24db97b0b18cb14b1fe8aad996f34a34963f2
kikimiriti <32511125+kikimiriti@users.noreply.github.com>
2017-10-18 11:56:06 +0300

    Add files via upload

---
+++ kiki95m.py
@@ -0,0 +1,8 @@
+print ("*******A progra that displays if a given number is greater than another number or lesser than another number**********")
+m = int(input("enter the first number\n"))
+n = int(input("enter the second number\n"))
+if(m>n):
+    print(m ,"is greater", n
+          )
+if (m<n):
+    print (m ,"is less" , n)
